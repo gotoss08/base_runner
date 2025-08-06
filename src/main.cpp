79ce8f638a68fbeab894c99d2c84cf9d62352ff0
Pavel Pereverzev <gotoss08@gmail.com>
2025-08-07 03:34:40 +0500

    add version.h to control versions

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 
 #include "App.h"
 #include "Engine.h"
+#include "version.h"
 
 int main(int, char**) {
-	auto engine = std::make_unique<Engine>(1280, 720, "Base Runner");
+	const auto engine = std::make_unique<Engine>(1280, 720, "BaseRunner " BASE_RUNNER_VERSION);
 	auto app = std::make_unique<App>(*engine);
 	engine->run(std::move(app));
 	return 0;
---
+++ src/version.h
@@ -0,0 +1,2 @@
+#pragma once
+#define BASE_RUNNER_VERSION "0.1"
